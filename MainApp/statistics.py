e501287a08ad47f08011c5b469c67bfb962a3b3d
Danila <dikrivenkov@edu.hse.ru>
2019-03-12 18:27:29 +0300

    smthing thing

--- MainApp/statistics.py
+++ MainApp/statistics.py
@@ -14,12 +14,12 @@ def max_metric(logs: '[[{},{}...]...]', path):
                 if arr['label'] in counts:
                     counts[arr['label']] += 1
         col = counts.most_common(len(counts.values()))
-        gen_maxMetric(col, path)
+        gen_max_metric(col, path)
     except Exception():
         print('WRONG FORMAT')
 
 
-def gen_maxMetric(counts: '[(),()...]', path):
+def gen_max_metric(counts: '[(),()...]', path):
     arr = []
     for i, count in enumerate(counts):
         if count[1] != 0:
@@ -28,12 +28,12 @@ def gen_maxMetric(counts: '[(),()...]', path):
             break
     arr = np.array(arr).reshape(-1, 1)
     count = list([x[0] for x in counts])
-    df = pd.DataFrame(arr[:len(arr), 0], index=count[:len(arr)], columns=['count'])
+    df = pd.DataFrame(arr[: len(arr), 0], index=count[: len(arr)], columns=['count'])
     df.index.name = 'objects'
     df.to_csv(path, encoding='utf-8')
 
 
-def median_metric(logs:'[[{},{}...]...]', path):
+def median_metric(logs: '[[{},{}...]...]', path):
     try:
         counts = {}
         for log in logs:
@@ -43,30 +43,19 @@ def median_metric(logs:'[[{},{}...]...]', path):
                     counts[arr['label']][1] += arr['confidence']
                 else:
                     counts[arr['label']] = [1, arr['confidence']]
-        gen_medianMetric(counts, path)
+        gen_median_metric(counts, path)
     except Exception():
         print('WRONG FORMAT')
 
-def gen_medianMetric(counts:'[(),()...]', path):
+def gen_median_metric(counts: '[(),()...]', path):
     arr = []
     for ch in counts:
-        counts[ch][1] = counts[ch][1]/counts[ch][0] # *counts[ch][0]
+        counts[ch][1] = counts[ch][1] / counts[ch][0] # *counts[ch][0]
         arr.extend(counts[ch])
-    arr = np.array(arr).reshape(-1,2)
+    arr = np.array(arr).reshape(-1, 2)
     df = pd.DataFrame(arr, index=counts.keys(), columns=['count', 'per'])
     df.sort_values(by=['count'])
     df['per'] = df['per'].map('{:.2f}'.format)
     df['count'] = df['count'].astype(int)
     df.index.name = 'objects'
     df.to_csv(path, encoding='utf-8')
-
-
-
-
-
-
-
-
-
-
-
