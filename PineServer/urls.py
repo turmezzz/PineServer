24620c25da8c70434fa8a239a8a2417ff6477c48
Danila <dikrivenkov@edu.hse.ru>
2019-03-14 14:34:22 +0300

    gitignore_abs

--- PineServer/urls.py
+++ PineServer/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+import os
+from django.conf import settings
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'admin/', admin.site.urls),
